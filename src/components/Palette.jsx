b121667400dbe979d5802dbb03bd988734a0d402
Sherelin Thomas <31187056+Sherelint@users.noreply.github.com>
2022-02-22 12:58:02 -0500

    slider and Slider Style

--- src/components/Palette.jsx
+++ src/components/Palette.jsx
@@ -1,16 +1,30 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import ColorBox from "./ColorBox";
 import { PalettesContext } from "../context/SeedColorsContext";
-import usePalette from "../hooks/usePalette";
 import "../styles/Palette.css";
+import { generatePalette } from "../helpers/ColorHelpes";
+import Slider, { Range } from "rc-slider";
+import "rc-slider/assets/index.css";
 export default function Palette(props) {
   const { palettes } = useContext(PalettesContext);
-  const { colors } = palettes[4];
+  const { colors } = generatePalette(palettes[4]);
+  const [level, setLevel] = useState(500);
+
   return (
     <div className="Palette">
+      <div className="slider">
+        <Slider
+          defaultValue={level}
+          min={100}
+          max={900}
+          step={100}
+          onAfterChange={(e) => setLevel({ level: e.target.value })}
+        />
+      </div>
+
       <div className="Palette-colors">
-        {colors.map((color) => (
-          <ColorBox background={color.color} name={color.name} />
+        {colors[level].map((color) => (
+          <ColorBox background={color.hex} name={color.name} />
         ))}
       </div>
     </div>
--- src/context/SeedColorsContext.jsx
+++ src/context/SeedColorsContext.jsx
@@ -1,4 +1,5 @@
 import { createContext } from "react";
+
 const palettes = [
   {
     paletteName: "Material UI Colors",
