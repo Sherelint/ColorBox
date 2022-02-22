f42fbb316b6cf2b5e1ff7796401ea840bf1a3474
Sherelin Thomas <sherelinthomasf@gmail.com>
2022-02-21 21:39:12 -0500

    more CSS for the Copy Screen

--- src/components/ColorBox.jsx
+++ src/components/ColorBox.jsx
@@ -1,17 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/ColorBox.css";
 import CopyToClipboard from "react-copy-to-clipboard";
 export default function ColorBox(props) {
+  const { background, name } = props;
+  const [copied, setCopied] = useState(false);
+  const changeCopyState = () => {
+    setCopied(!copied), () => setTimeout(() => setCopied(!copied), 150);
+  };
   return (
-    <CopyToClipboard text={props.background}>
-      <div style={{ background: props.background }} className="ColorBox">
+    <CopyToClipboard text={background} onCopy={changeCopyState}>
+      <div style={{ background }} className="ColorBox">
+        <div
+          style={{ background }}
+          className={`copy-overlay ${copied && "show"}`}
+        />
+        <div className={`copy-msg ${copied && "show"}`}>
+          <h1>Copied!</h1>
+          <p>{background}</p>
+        </div>
         <div className="copy-container">
           <div className="box-container">
-            <span>{props.name}</span>
+            <span>{name}</span>
           </div>
           <button className="copy-button">Copy</button>
-          <span className="see-more">MORE</span>
         </div>
+        <span className="see-more">MORE</span>
       </div>
     </CopyToClipboard>
   );
