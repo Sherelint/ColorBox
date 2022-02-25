18f3d21b9328924fbc33d5d34e6fa83575f8fa40
Sherelin Thomas <31187056+Sherelint@users.noreply.github.com>
2022-02-25 13:32:45 -0500

    styling miniPalette

--- src/components/MiniPalette.jsx
+++ src/components/MiniPalette.jsx
@@ -1,17 +1,24 @@
 import React from "react";
 import { withStyles } from "@mui/styles";
 import styles from "../styles/MiniPaletteStyles";
-
+import { Link } from "react-router-dom";
 function MiniPalette(props) {
-  const { classes, paletteName, emoji } = props;
+  const { classes, paletteName, emoji, colors } = props;
   return (
     <div className={classes.root}>
       <div className={classes.colors}>
-        <h5 className={classes.title}>
-          {paletteName}
-          <span className={classes.emoji}>{emoji}</span>
-        </h5>
+        {colors.map((color) => (
+          <div
+            key={color.name}
+            className={classes.miniColor}
+            style={{ backgroundColor: color.color }}
+          ></div>
+        ))}
       </div>
+      <h5 className={classes.title}>
+        {paletteName}
+        <span className={classes.emoji}>{emoji}</span>
+      </h5>
     </div>
   );
 }
--- src/components/PaletteList.jsx
+++ src/components/PaletteList.jsx
@@ -1,14 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import MiniPalette from "./MiniPalette";
-export default function PaletteList(props) {
-  const { palettes } = props;
+import { withStyles } from "@mui/styles";
+import styles from "../styles/PaletteListStyles";
+function PaletteList(props) {
+  const { palettes, classes } = props;
   return (
-    <div>
-      <h1>React Colors</h1>
-      {palettes.map((palette) => (
-        <MiniPalette {...palette} key={palette.id} />
-      ))}
+    <div className={classes.root}>
+      <div className={classes.container}>
+        <nav className={classes.nav}>
+          <h1>React Colors</h1>
+        </nav>
+        <div className={classes.palettes}>
+          {palettes.map((palette) => (
+            <MiniPalette {...palette} key={palette.id} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
+export default withStyles(styles)(PaletteList);
