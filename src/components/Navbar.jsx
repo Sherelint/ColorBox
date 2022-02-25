af89d60bc0965390772be90b86480b81f430eafc
Sherelin Thomas <31187056+Sherelint@users.noreply.github.com>
2022-02-25 14:08:23 -0500

    Fix the Slider uncontrolled issue

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,6 +20,7 @@ function Navbar(props) {
   };
 
   const { classes } = props;
+
   return (
     <header className={classes.Navbar}>
       <button className={classes.logo}>
@@ -31,7 +32,7 @@ function Navbar(props) {
         <span>Level:{level}</span>
         <div className="slider">
           <Slider
-            defaultValue={level}
+            value={level}
             min={100}
             max={900}
             step={100}
